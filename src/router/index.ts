501c81fcb43d0b7764a02d55c0c89c342e326795
Alex <fornikaras7@gmail.com>
2025-04-26 01:48:49 +0500

    feat: add basic files structure

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import WeatherView from '../features/weather/WeatherView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'weather',
+      component: WeatherView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/gallery',
+      name: 'gallery',
+      component: () => import('../features/gallery/GalleryView.vue'),
+    },
+    {
+      path: '/music',
+      name: 'music',
+      component: () => import('../features/music/MusicView.vue'),
     },
   ],
 })
