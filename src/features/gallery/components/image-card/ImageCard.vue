60fc9e8579d8d6889f8d33c5723464c435991c0b
Alex <fornikaras7@gmail.com>
2025-04-27 12:37:13 +0500

    feat: add download button styles , and linear gradient on hover

--- src/features/gallery/components/image-card/ImageCard.vue
+++ src/features/gallery/components/image-card/ImageCard.vue
@@ -11,7 +11,10 @@ const handleClick = useDownloadImage(downloadLink)
 <template>
   <div class="card">
     <img :src="imgSrc" alt="" class="card-img" />
-    <button @click="handleClick" class="download-btn">Download</button>
+    <div class="gradient-overlay"></div>
+    <div class="card-buttons">
+      <button @click="handleClick" class="download-btn">Download</button>
+    </div>
   </div>
 </template>
 
@@ -23,27 +26,52 @@ const handleClick = useDownloadImage(downloadLink)
   height: auto;
   position: relative;
   overflow: hidden;
+  cursor: pointer;
 }
 .card-img {
   width: 100%;
   height: 100%;
   border-radius: 5px;
 }
-.description {
+.gradient-overlay {
   position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  pointer-events: none;
+  opacity: 0;
+  background: linear-gradient(to bottom, rgb(74 72 72 / 30%), rgba(0, 0, 0, 0.6));
+  transition: opacity 0.3s ease;
+  z-index: 1;
 }
-.download-btn {
+
+.card-buttons {
   position: absolute;
   bottom: 0;
-  left: 0;
-  opacity: 0;
-  transition:
-    opacity 0.7s ease,
-    transform 0.7s ease;
-  transform: translateY(50px);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  transform: translateY(100%);
+  transition: transform 0.3s ease;
+  padding: 10px;
+  z-index: 2;
 }
-.card:hover .download-btn {
+
+.card:hover .gradient-overlay {
   opacity: 1;
+}
+
+.card:hover .card-buttons {
   transform: translateY(0);
+}
+.download-btn {
+  border-radius: 2px;
+  border: none;
+  padding: 7px;
+  cursor: pointer;
+  background-color: inherit;
+  color: white;
+  font-weight: 600;
 }
 </style>
